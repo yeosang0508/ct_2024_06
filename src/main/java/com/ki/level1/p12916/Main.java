de76d5091f294cd963f4e254856a3ca8661bcd52
yeosang0508 <yeosang0508@gmail.com>
2024-06-19 13:59:35 +0900

    programmers_level1_p12916

---
+++ src/main/java/com/ki/level1/p12916/Main.java
@@ -0,0 +1,35 @@
+package com.ki.level1.p12916;
+
+public class Main {
+
+
+}
+
+class Solution {
+    boolean solution(String s) {
+
+        s = s.toLowerCase();
+        String[] str = s.split("");
+
+        int p = 0;
+        int y = 0;
+        for (int i = 0; i < str.length; i++) {
+            if (str[i].equals("p")) {
+                p++;
+            }
+        }
+
+        for (int i = 0; i < str.length; i++) {
+            if (str[i].equals("y")) {
+                y++;
+            }
+        }
+
+
+        if (p == y) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/test/java/com/ki/level1/p12916/Tests.java
@@ -0,0 +1,19 @@
+package com.ki.level1.p12916;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Tests {
+
+    @Test
+    @DisplayName("pPoooyY => true")
+    void t1() {assertThat(new Solution().solution("pPoooyY")).isEqualTo(true);}
+
+    @Test
+    @DisplayName("Pyy => false")
+    void t2() {assertThat(new Solution().solution("Pyy")).isEqualTo(false);}
+
+
+}
