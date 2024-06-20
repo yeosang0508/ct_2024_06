defc8e1e416b3bd23fcb0fa77d4f0179030b5ea4
yeosang0508 <yeosang0508@gmail.com>
2024-06-20 23:36:52 +0900

    programmers_level1_x만큼 간격이 있는 n개의 숫자

--- src/main/java/level1/p12954/Main.java
+++ src/main/java/level1/p12954/Main.java
@@ -1,29 +1,16 @@
 package level1.p12954;
 
 public class Main {
-    public static void main(String[] args) {
-        int x = 2;
-        int n = 5;
-        long[] answer = new long[n];
-
-
-        for (int i = 0; i < n; i++) {
-            answer[i] = x;
-            x += x;
-        }
-
-        System.out.println(answer);
-    }
 }
 
 
 class Solution {
     public long[] solution(int x, int n) {
         long[] answer = new long[n];
-
-        for (int i = 0; i < n; i++) {
-            answer[i] = x;
-            x += x;
+        long num = x;
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = num;
+            num += x;
         }
         return answer;
     }
--- src/test/java/level1/p12954/Tests.java
+++ src/test/java/level1/p12954/Tests.java
@@ -9,15 +9,15 @@ public class Tests {
 
     @Test
     @DisplayName("2, 5 => [2,4,6,8,10]")
-    void t1(){assertThat(new Solution().solution(2, 5)).isEqualTo(new int[] {2,4,6,8,10});}
+    void t1(){assertThat(new Solution().solution(2, 5)).isEqualTo(new long[] {2,4,6,8,10});}
 
     @Test
     @DisplayName("4, 3 => [4,8,12]")
-    void t2(){assertThat(new Solution().solution(2, 5)).isEqualTo(new int[] {2,4,6,8,10});}
+    void t2(){assertThat(new Solution().solution(2, 5)).isEqualTo(new long[] {2,4,6,8,10});}
 
     @Test
     @DisplayName("-4,2 => [-4,-8]")
-    void t3(){assertThat(new Solution().solution(2, 5)).isEqualTo(new int[] {2,4,6,8,10});}
+    void t3(){assertThat(new Solution().solution(2, 5)).isEqualTo(new long[] {2,4,6,8,10});}
 
 
 }
