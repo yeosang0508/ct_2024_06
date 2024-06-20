d036ee13dcb3fc8a20e60c63552762d92bc08637
yeosang0508 <yeosang0508@gmail.com>
2024-06-20 16:41:43 +0900

    programmers_level1_x만í큼 간격이 있는 n개의 숫자_fail

---
+++ src/test/java/level1/p12954/Tests.java
@@ -0,0 +1,23 @@
+package level1.p12954;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Tests {
+
+    @Test
+    @DisplayName("2, 5 => [2,4,6,8,10]")
+    void t1(){assertThat(new Solution().solution(2, 5)).isEqualTo(new int[] {2,4,6,8,10});}
+
+    @Test
+    @DisplayName("4, 3 => [4,8,12]")
+    void t2(){assertThat(new Solution().solution(2, 5)).isEqualTo(new int[] {2,4,6,8,10});}
+
+    @Test
+    @DisplayName("-4,2 => [-4,-8]")
+    void t3(){assertThat(new Solution().solution(2, 5)).isEqualTo(new int[] {2,4,6,8,10});}
+
+
+}
