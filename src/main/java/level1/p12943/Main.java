17ed658d9eb65726ab0ad221fbf22f20ecacc8a1
yeosang0508 <yeosang0508@gmail.com>
2024-06-21 23:55:23 +0900

    programmers_level1_콜라츠 추측

---
+++ src/main/java/level1/p12943/Main.java
@@ -0,0 +1,30 @@
+package level1.p12943;
+
+public class Main {
+
+}
+
+class Solution {
+    public int solution(long num) {
+        int count = 0;
+
+        if(num == 1) return 0;
+
+        while (num != 1){
+            if(num % 2 == 0) {
+                num = num / 2;
+            }else if (num % 2 == 1){
+                num = num * 3 + 1;
+            }
+
+            count++;
+
+            if(count >= 500) {
+                count = -1;
+                break;
+            }
+
+        }
+        return count;
+    }
+}
