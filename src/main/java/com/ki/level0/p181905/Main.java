edbfc92d3966a6b451ccbf4a5e2c4031bd722c21
yeosang0508 <yeosang0508@gmail.com>
2024-08-14 23:36:06 +0900

    programmers_level0_문자열 뒤집기

---
+++ src/main/java/com/ki/level0/p181905/Main.java
@@ -0,0 +1,15 @@
+package com.ki.level0.p181905;
+
+public class Main {
+}
+
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        String answer = my_string.substring(0, s);
+        for (int i = e; i >= s; i--) {
+            answer += my_string.charAt(i);
+        }
+        answer += my_string.substring(e + 1);
+        return answer;
+    }
+}
